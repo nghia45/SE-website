2bed7980769b1d365a873daaedd034dc4e264214
phanducmanhk65 <phanducmanh2812002@gmail.com>
2022-04-27 20:38:44 +0700

    up de lam giao dien

---
+++ BTL_CNPM-20220427T040038Z-001/BTL_CNPM/pages/main/danhmuc.php
@@ -0,0 +1,55 @@
+<h3>Addidas</h3>
+<?php
+
+?>
+                <ul class="product_list">
+                    <li>
+                        <a href="">
+                        <img src="img/giayadd1.jpg" class="rounded" alt="Cinque Terre">
+                        <p class="title_product">Tên sản phẩm: Giày Addidas</p>
+                        <p class="price_product">Giá 300k</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                        <img src="img/giayadd1.jpg" class="rounded" alt="Cinque Terre">
+                        <p class="title_product">Tên sản phẩm: Giày Addidas</p>
+                        <p class="price_product">Giá 300k</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                        <img src="img/giayadd1.jpg">
+                        <p class="title_product">Tên sản phẩm: Giày Addidas</p>
+                        <p class="price_product">Giá 300k</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                        <img src="img/giayadd1.jpg">
+                        <p class="title_product">Tên sản phẩm: Giày Addidas</p>
+                        <p class="price_product">Giá 300k</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                        <img src="img/giayadd1.jpg">
+                        <p class="title_product">Tên sản phẩm: Giày Addidas</p>
+                        <p class="price_product">Giá 300k</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                        <img src="img/giayadd1.jpg">
+                        <p class="title_product">Tên sản phẩm: Giày Addidas</p>
+                        <p class="price_product">Giá 300k</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                        <img src="img/giayadd1.jpg">
+                        <p class="title_product">Tên sản phẩm: Giày Addidas</p>
+                        <p class="price_product">Giá 300k</p>
+                        </a>
+                    </li>
+                </ul>
---
+++ BTL_CNPM-20220427T040038Z-001/BTL_CNPM/pages/sidebar/sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+            <ul class="list_sidebar">
+                <li><a href="index.php?quanly=danhmucsanpham&id=1">Addidas</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=2">Nike</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham&id=3">Bitis</a></li>
+            </ul>
+</div>
