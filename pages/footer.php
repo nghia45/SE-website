10a8c6a7cfa07eefb344ec4ec86f14afebbadf6c
ngocphung <81069876+Ngocphung66@users.noreply.github.com>
2022-05-15 10:20:24 +0700

    Update footer.php

--- pages/footer.php
+++ pages/footer.php
@@ -2,52 +2,103 @@
               <div class="container py-5">
                 <div class="row py-4">
                   <div class="col-lg-4 col-md-6 mb-4 mb-lg-0"><img src="img/logo.png" alt="" width="180" class="mb-3">
-                    <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
-                    <ul class="list-inline mt-4">
-                      <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter"></i></a></li>
-                      <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fa fa-facebook"></i></a></li>
-                      <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fa fa-instagram"></i></a></li>
-                      <li class="list-inline-item"><a href="#" target="_blank" title="pinterest"><i class="fa fa-pinterest"></i></a></li>
-                      <li class="list-inline-item"><a href="#" target="_blank" title="vimeo"><i class="fa fa-vimeo"></i></a></li>
+                    <p class="fw-bolder">Impossible is nothing.</p>
+                  </div>
+                  <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                    <h6 class="text-uppercase font-weight-bold mb-4">Sản phẩm</h6>
+                    <ul class="list-unstyled mb-0">
+                      <li class="mb-2"><a href="#" class="text-muted">Giày nam</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Giày nữ</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Hàng mới về</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Bộ sưu tập</a></li>
                     </ul>
                   </div>
                   <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                    <h6 class="text-uppercase font-weight-bold mb-4">Shop</h6>
+                    <h6 class="text-uppercase font-weight-bold mb-4">Thể thao</h6>
                     <ul class="list-unstyled mb-0">
-                      <li class="mb-2"><a href="#" class="text-muted">For Women</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">For Men</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Stores</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Our Blog</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Bóng đá</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Bóng rổ</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Đánh golf</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Chạy bộ</a></li>
                     </ul>
                   </div>
                   <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                    <h6 class="text-uppercase font-weight-bold mb-4">Company</h6>
+                    <h6 class="text-uppercase font-weight-bold mb-4">Hỗ trợ</h6>
                     <ul class="list-unstyled mb-0">
-                      <li class="mb-2"><a href="#" class="text-muted">Login</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Register</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Wishlist</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Our Products</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Tư vấn khách hàng</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Trả hàng</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Hoàn tiền</a></li>
+                      <li class="mb-2"><a href="#" class="text-muted">Khuyến mãi</a></li>
                     </ul>
                   </div>
-                  <div class="col-lg-4 col-md-6 mb-lg-0">
-                    <h6 class="text-uppercase font-weight-bold mb-4">Newsletter</h6>
-                    <p class="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At itaque temporibus.</p>
-                    <div class="p-1 rounded border">
-                      <div class="input-group">
-                        <input type="email" placeholder="Enter your email address" aria-describedby="button-addon1" class="form-control border-0 shadow-0">
-                        <div class="input-group-append">
-                          <button id="button-addon1" type="submit" class="btn btn-link"><i class="fa fa-paper-plane"></i></button>
-                        </div>
-                      </div>
-                    </div>
                   </div>
                 </div>
               </div>
-          
-              <!-- Copyrights -->
               <div class="bg-light py-4">
-                <div class="container text-center">
-                  <p class="text-muted mb-0 py-2">© 2019 Bootstrapious All rights reserved.</p>
-                </div>
-              </div>
+            <footer class="text-center text-white" style="background-color: #f1f1f1;">
+      <!-- Grid container -->
+      <div class="container pt-4">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+          <!-- Facebook -->
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-facebook-f"></i
+          ></a>
+
+          <!-- Twitter -->
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-twitter"></i
+          ></a>
+
+          <!-- Google -->
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-google"></i
+          ></a>
+
+          <!-- Instagram -->
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-instagram"></i
+          ></a>
+
+          <!-- Linkedin -->
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-linkedin"></i
+          ></a>
+          <!-- Github -->
+          <a
+            class="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="#!"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i class="fab fa-github"></i
+          ></a>
+        </section>
+        <!-- Section: Social media -->
+  </div>
+				<div class="container text-center">
+				  <p class="text-muted mb-0 py-2">© 2022 Công ty TNHH Adidas Việt Nam.</p>
+				</div>
+			  </div>
+              <!-- Copyrights -->
+              
             </footer>
