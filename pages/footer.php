0206a86c3c478154f18cefbcf7bf5bd4ce296fb1
ngocphung66 <ngocphung2662@gmail.com>
2022-05-19 19:30:13 +0700

    update footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,100 +1,57 @@
-<footer class="bg-white">
-              <div class="container py-5">
-                <div class="row py-4">
-                  <div class="col-lg-4 col-md-6 mb-4 mb-lg-0"><img src="img/logo.png" alt="" width="180" class="mb-3">
-                    <p class="fw-bolder">Impossible is nothing.</p>
-                  </div>
-                  <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                    <h6 class="text-uppercase font-weight-bold mb-4">Sản phẩm</h6>
-                    <ul class="list-unstyled mb-0">
-                      <li class="mb-2"><a href="#" class="text-muted">Giày nam</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Giày nữ</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Hàng mới về</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Bộ sưu tập</a></li>
-                    </ul>
-                  </div>
-                  <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                    <h6 class="text-uppercase font-weight-bold mb-4">Thể thao</h6>
-                    <ul class="list-unstyled mb-0">
-                      <li class="mb-2"><a href="#" class="text-muted">Bóng đá</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Bóng rổ</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Đánh golf</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Chạy bộ</a></li>
-                    </ul>
-                  </div>
-                  <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
-                    <h6 class="text-uppercase font-weight-bold mb-4">Hỗ trợ</h6>
-                    <ul class="list-unstyled mb-0">
-                      <li class="mb-2"><a href="#" class="text-muted">Tư vấn khách hàng</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Trả hàng</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Hoàn tiền</a></li>
-                      <li class="mb-2"><a href="#" class="text-muted">Khuyến mãi</a></li>
-                    </ul>
-                  </div>
-                  </div>
-                </div>
-              </div>
-              <div class="bg-light py-4">
-            <footer class="text-center text-white" style="background-color: #f1f1f1;">
-      <!-- Grid container -->
-      <div class="container pt-4">
-        <!-- Section: Social media -->
-        <section class="mb-4">
-          <!-- Facebook -->
-          <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="#!"
-            role="button"
-            data-mdb-ripple-color="dark"
-            ><i class="fab fa-facebook-f"></i
-          ></a>
-          <!-- Twitter -->
-          <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="#!"
-            role="button"
-            data-mdb-ripple-color="dark"
-            ><i class="fab fa-twitter"></i
-          ></a>
-          <!-- Google -->
-          <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="#!"
-            role="button"
-            data-mdb-ripple-color="dark"
-            ><i class="fab fa-google"></i
-          ></a>
-          <!-- Instagram -->
-          <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="#!"
-            role="button"
-            data-mdb-ripple-color="dark"
-            ><i class="fab fa-instagram"></i
-          ></a>
-          <!-- Linkedin -->
-          <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="#!"
-            role="button"
-            data-mdb-ripple-color="dark"
-            ><i class="fab fa-linkedin"></i
-          ></a>
-          <!-- Github -->
-          <a
-            class="btn btn-link btn-floating btn-lg text-dark m-1"
-            href="#!"
-            role="button"
-            data-mdb-ripple-color="dark"
-            ><i class="fab fa-github"></i
-          ></a>
-        </section>
-        <!-- Section: Social media -->
-  </div>
-				<div class="container text-center">
-				  <p class="text-muted mb-0 py-2">© 2022 SplashShoe Limited</p>
+<footer class="footer-distributed">
+     <link rel="stylesheet" href="./asserts/css/footer.css">
+			<div class="footer-left">
+
+        <a class="navbar-brand" href="#">
+								<img src="./asserts/img/logo.png" alt="">
+				</a>
+
+				<p class="footer-links">
+					<a href="#" class="link-1">Home</a>
+					
+					<a href="#">Contact</a>
+					
+					<a href="#">About</a>
+				</p>
+
+				<p class="footer-company-name">Công ty TNHH Adidas © 2022</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p>144 Xuân Thủy, Cầu Giấy, Hà Nội</p>
 				</div>
-			  </div>
-              <!-- Copyrights -->
-              
-            </footer>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+1.555.555.5555</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">support@adidas.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Our Slogan</span>
+					“Impossible is nothing. Put your dreams on your feet to guide them into reality”
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fab fa-facebook"></i></a>
+					<a href="#"><i class="fab fa-twitter"></i></a>
+					<a href="#"><i class="fab fa-linkedin"></i></a>
+					<a href="#"><i class="fab fa-github"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
