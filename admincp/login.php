861f5a446a52a1c50c753d22aff6ed8b8ebb3539
nghia <godresiv@gmail.com>
2022-05-19 12:44:58 +0700

    change admin login form

--- admincp/login.php
+++ admincp/login.php
@@ -1,58 +1,62 @@
 <?php
-    session_start();
-    include('config/config.php');
-    if(isset($_POST['dangnhap'])) {
-        $tk = $_POST['username'];
-        $mk = md5($_POST['password']);
-        $sql = "SELECT * FROM tb_admin WHERE username = '".$tk."' AND password = '".$mk."' LIMIT 1";
-        $row = mysqli_query($mysqli,$sql);
-        $count = mysqli_num_rows($row);
-        if($count > 0) {
-            $_SESSION['dangnhap'] = $tk;
-            header('Location:index.php');
-        } else {
-            echo '<script>alert("Tài khoản hoặc mật khẩu không đúng, vui lòng đăng nhập lại")</script>';
-            header('Locaion:login.php');
-        }
-    }
+session_start();
+include('config/config.php');
+if(isset($_POST['dangnhap'])) {
+$tk = $_POST['username'];
+$mk = md5($_POST['password']);
+$sql = "SELECT * FROM tb_admin WHERE username = '".$tk."' AND password = '".$mk."' LIMIT 1";
+$row = mysqli_query($mysqli,$sql);
+$count = mysqli_num_rows($row);
+if($count > 0) {
+$_SESSION['dangnhap'] = $tk;
+header('Location:index.php');
+} else {
+echo '<script>alert("Tài khoản hoặc mật khẩu không đúng, vui lòng đăng nhập lại")</script>';
+header('Locaion:login.php');
+}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng nhập Admincp</title>
-    <style type="text/css">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Đăng nhập Admincp</title>
+        <link rel="stylesheet" type="text/css" href="../asserts/css/bootstrap.css">
+        <style type="text/css">
         body {
-            background: #f2f2f2;
+        background: #f2f2f2;
         }
-        .wrapper-login {
-            width: 15%;
-            margin: 0 auto;
+        .form-dangnhap{
+        width: 50%;
+        padding: 20px;
+        margin: auto;
+        border: 1px solid;
+        border-color: black;
+        border-radius: 10px;
         }
-    </style>
-</head>
-<body>
-      <div class="wrapper-login">
-    <form action="" method="post" autocomplete="off">
-      <table class="table-login" border="1" style="text-align: center;border-collapse: collapse;">
-          <tr>
-              <td colspan="2"><h3>Đăng nhập Admin</h3></td>
-          </tr>
-          <tr>
-              <td>Tài khoản</td>
-              <td><input type="text" name="username"></td>
-          </tr>
-          <tr>
-              <td>Mật khẩu</td>
-              <td><input type="password" name="password"></td>
-          </tr>
-          <tr>
-              <td colspan="2"><input type="submit" name="dangnhap" value="Đăng nhập"></td>
-          </tr>
-      </table>
-      </form>
-    </div>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-      </body>
+        </style>
+    </head>
+    <body>
+        <form action="" method="post" class="form-dangnhap">
+            <p>Đăng nhập admin</p>
+            <div class="row align-items-center mb-3">
+                <div class="col-6">
+                    <label for="exampleInputEmail1" class="form-label">Tài khoản</label>
+                    <input type="text" name="username" class="form-control">
+                </div>
+                <div class="col-6">
+                    <label for="exampleInputEmail1" class="form-label">Mật khẩu</label>
+                    <input type="password" name="password" class="form-control" autocomplete="off">
+                </div>
+            </div>
+            <div class="align-items-center mb-3">
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-primary" name="dangnhap" value="Đăng nhập">Đăng nhập</button>
+                </div>
+            </div>
+        </form>
+        <script src="..\asserts\js\bootstrap.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </body>
 </html>
