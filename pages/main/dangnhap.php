4c74242e146420655df481c81138f8b4d4370e29
phanducmanhk65 <80315499+phanducmanhk65@users.noreply.github.com>
2022-05-27 15:01:18 +0700

    Update dangnhap.php
    tao sửa nè zừa lòng chưa

--- pages/main/dangnhap.php
+++ pages/main/dangnhap.php
@@ -1,3 +1,22 @@
+<?php
+            if (isset($_POST['dangnhap'])) {
+                $tk = $_POST['email'];
+                $mk = ($_POST['password']);
+                $sql = "SELECT * FROM tb_dangki WHERE email = '" . $tk . "' AND matkhau = '" . $mk . "' LIMIT 1";
+                $row = mysqli_query($mysqli, $sql);
+                $count = mysqli_num_rows($row);
+                if ($count > 0 && $tk != NULL && $mk != NULL) {
+                    $row_data = mysqli_fetch_array($row);
+                    $_SESSION['dangki'] = $row_data['tenkhachhang'];
+                    $_SESSION['email'] = $row_data['email'];
+                    $_SESSION['id_khachhang'] = $row_data['id_dangki'];
+                    header('Location:index.php');
+                } else {
+                    echo '<p style="color: red">Mật khẩu hoặc email sai, vui lòng nhập lại</p>';
+                }
+            }
+            ?>
+
 <h3>Đăng nhập</h3>
 <form action="" method="post" class="form-dangnhap">
     <div class="row align-items-center mb-3">
@@ -17,23 +36,6 @@
         <div class="col-auto">
             <a class="nav-link" href="index.php?quanly=dangki">Đăng kí</a>
         </div>
-        <?php
-            if (isset($_POST['dangnhap'])) {
-                $tk = $_POST['email'];
-                $mk = ($_POST['password']);
-                $sql = "SELECT * FROM tb_dangki WHERE email = '" . $tk . "' AND matkhau = '" . $mk . "' LIMIT 1";
-                $row = mysqli_query($mysqli, $sql);
-                $count = mysqli_num_rows($row);
-                if ($count > 0 && $tk != NULL && $mk != NULL) {
-                    $row_data = mysqli_fetch_array($row);
-                    $_SESSION['dangki'] = $row_data['tenkhachhang'];
-                    $_SESSION['email'] = $row_data['email'];
-                    $_SESSION['id_khachhang'] = $row_data['id_dangki'];
-                    header('Location:index.php?quanly=giohang');
-                } else {
-                    echo '<p style="color: red">Mật khẩu hoặc email sai, vui lòng nhập lại</p>';
-                }
-            }
-            ?>
+      
     </div>
 </form>
