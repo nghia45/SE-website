af51fa718306b3742da76969837bc6e8a0b85c78
phanducmanhk65 <80315499+phanducmanhk65@users.noreply.github.com>
2022-05-28 11:31:14 +0700

    Update login.php

--- admincp/login.php
+++ admincp/login.php
@@ -45,24 +45,7 @@ header('Locaion:login.php');
             <div class="align-items-center mb-3">
                 <div class="col-auto">
                     <button type="submit" class="btn btn-primary" name="dangnhap" value="Đăng nhập">Đăng nhập</button>
-                    <?php
-                    if(isset($_POST['dangnhap'])) {
-                    $tk = $_POST['email'];
-                    $mk = md5($_POST['password']);
-                    $sql = "SELECT * FROM tb_dangki WHERE email = '".$tk."' AND matkhau = '".$mk."' LIMIT 1";
-                    $row = mysqli_query($mysqli,$sql);
-                    $count = mysqli_num_rows($row);
-                    if($count > 0) {
-                    $row_data = mysqli_fetch_array($row);
-                    $_SESSION['dangki'] = $row_data['tenkhachhang'];
-                    $_SESSION['email'] = $row_data['email'];
-                    $_SESSION['id_khachhang'] = $row_data['id_dangki'];
-                    header('Location:index.php?quanly=giohang');
-                    } else {
-                    echo '<p style="color: red">Mật khẩu hoặc email sai, vui lòng nhập lại</p>';
-                    }
-                    }
-                    ?>
+                   
                 </div>
             </div>
         </form>
